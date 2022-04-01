61ded19b6279ec7f5178943c24b80b0103cb3aa2
Rafael_Madureira <rafinhakd4@gmail.com>
2022-04-01 15:45:56 -0300

    Add files via upload

---
+++ dist/js/app.js
@@ -0,0 +1,6 @@
+import { Negociacao } from "./models/negociacao.js";
+const negociacao = new Negociacao(new Date(), 10, 100);
+//console.log(negociacao);
+//negociacao.quantidade=1000;
+//console.log(negociacao);
+console.log(negociacao.volume);
