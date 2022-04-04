13b79a82d976eae4f282601ac62f5949e4563062
Rafael_Madureira <rafinhakd4@gmail.com>
2022-04-04 10:54:13 -0300

    Add files via upload

--- dist/js/controllers/nogociacao-controller.js
+++ dist/js/controllers/nogociacao-controller.js
@@ -1,13 +1,16 @@
 import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
 export class NegociacaoController {
     constructor() {
+        this.negociacoes = new Negociacoes();
         this.inputData = document.querySelector('#data');
         this.InputQuantidade = document.querySelector('#quantidade');
         this.InputValor = document.querySelector('#valor');
     }
     adiciona() {
         const negociacao = this.criaNegociacao();
-        console.log(negociacao);
+        this.negociacoes.adiciona(negociacao);
+        console.log(this.negociacoes.lista());
         this.limparFormulario();
     }
     criaNegociacao() {
